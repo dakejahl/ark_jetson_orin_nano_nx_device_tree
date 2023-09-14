504013efb2353dca3bdc24795b3cb508544d58b7
Jacob Dahl <dahl.jakejacob@gmail.com>
2023-09-14 12:14:05 -0800

    renamed rbpcv2 to ark, everything works now

--- copy_all.sh
+++ copy_all.sh
@@ -2,8 +2,8 @@
 
 KERNEL_SRC_DT_PATH="/home/jake/code/ark/jetson_carrier/provisioning/kernel_source_build/Linux_for_Tegra/source/public/hardware/nvidia/platform/t23x/p3768/kernel-dts/"
 
-cp cvb/tegra234-camera-rbpcv2-imx219.dtsi $KERNEL_SRC_DT_PATH/cvb/
+cp cvb/tegra234-camera-ark-imx219.dtsi $KERNEL_SRC_DT_PATH/cvb/
 cp cvb/tegra234-p3768-0000-a0.dtsi $KERNEL_SRC_DT_PATH/cvb/
-cp cvb/tegra234-p3768-camera-rbpcv2-imx219.dtsi $KERNEL_SRC_DT_PATH/cvb/
+cp cvb/tegra234-p3768-camera-ark-imx219.dtsi $KERNEL_SRC_DT_PATH/cvb/
 
 
